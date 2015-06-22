4e86debfe7d71e3248c4cb70b6645057589b2908
Prince Oppong Boamah <regioths@gmail.com>
2015-06-22 13:24:05 +0000

    Created the script weekdays.sh . closes #5

---
+++ scripts/weekdays.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# weekdays.sh -- This script prints the names of weekdays
+
+# Author : prince Boamah<regioths@gmail.com>
+# Date : 22 June 2015
+
+echo "Sunday"
+echo "Monday"
+echo "Tuesday"
+echo "Wednesday"
+echo "Thursday"
+echo "Friday"
+echo "Saturday"
+
