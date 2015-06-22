6c72d43cda65bbc7b101b105f8637697afb9692a
Prince Oppong Boamah <regioths@gmail.com>
2015-06-22 16:30:24 +0000

    Created the script mkdir2.sh . closes #7

---
+++ scripts/mkdir2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# mkdir.sh -- This script asks the user for the name of a folder, and then creates it
+
+# Author : Prince Oppong Boamah<regioths@gmail.com>
+# Date : 22 June 2015
+
+echo -n "what's the name of the folder you wanted? "
+read name
+mkdir $name
+
