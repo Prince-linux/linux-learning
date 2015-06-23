67d6ab5d2ff887ce8be55068c12109c11ed4f75b
Prince Oppong Boamah <regioths@gmail.com>
2015-06-23 11:44:47 +0000

    Added years.sh. closes #6

---
+++ scripts/years.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# years.sh -- this is a program that creates the following years folder 2010, 2011, 2012, 2013, 2014, 2015 and its corresponding subfolders as accounts, hr and sales.
+#
+# Author : Prince Oppong Boamah<regioths@gmail.com>
+# Date : 22 June 2015
+
+ mkdir 2010 2011 2012 2013 2014 2015
+
+ cd 2010 
+ mkdir accounts hr sales
+ cd ..
+
+ cd 2011
+ mkdir accounts hr sales
+ cd ..
+
+ cd 2012
+ mkdir accounts hr sales
+ cd ..
+
+ cd 2013
+ mkdir accounts hr sales
+ cd ..
+
+ cd 2014
+ mkdir accounts hr sales
+ cd ..
+
+ cd 2015
+ mkdir accounts hr sales
+ cd..
