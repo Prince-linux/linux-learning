49e77b383e51bc18cf7cbda0f5da7110ab7cd115
Prince Oppong Boamah <regioths@gmail.com>
2015-06-24 06:53:32 +0000

    simplified the script years.sh . closes #11

--- scripts/years.sh
+++ scripts/years.sh
@@ -1,32 +1,20 @@
 #! /bin/bash
 
-# years.sh -- this is a program that creates the following years folder 2010, 2011, 2012, 2013, 2014, 2015 and its corresponding subfolders as accounts, hr and sales.
-#
-# Author : Prince Oppong Boamah<regioths@gmail.com>
-# Date : 22 June 2015
+for years in $(seq 2010 2015); do 
+    echo The script years.sh now prints the following year $years
+done
+    
+   for years in 2010 2011 2012 2013 2014 2015; do 
+       echo creating folder for $years
+       mkdir $years
+       cd $years
 
- mkdir 2010 2011 2012 2013 2014 2015
+       for dir in  accounts hr sales; do
+           mkdir $dir
+       done
 
- cd 2010 
- mkdir accounts hr sales
- cd ..
+       cd ..
+done
 
- cd 2011
- mkdir accounts hr sales
- cd ..
 
- cd 2012
- mkdir accounts hr sales
- cd ..
 
- cd 2013
- mkdir accounts hr sales
- cd ..
-
- cd 2014
- mkdir accounts hr sales
- cd ..
-
- cd 2015
- mkdir accounts hr sales
- cd..
