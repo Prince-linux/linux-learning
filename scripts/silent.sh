429d0548fcb28d09c11f563e53d50a3f7d79334f
Prince Oppong Boamah <regioths@gmail.com>
2015-06-26 15:12:59 +0000

    Created the Script silent.sh . closes #17

---
+++ scripts/silent.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# silent.sh -- This script prints the names of the days of the week to /dev/null. In otherwords the program prints out nothing.
+
+# Author : Prince Oppong Boamah<regioths@gmail.com>
+# Date : 26 June 2015
+
+echo "Sunday" > /dev/null
+echo "Monday" > /dev/null
+echo "Tuesday" > /dev/null
+echo "Wednesday" > /dev/null
+echo "Thursday" > /dev/null
+echo "Friday" > /dev/null
+echo "Saturday" > /dev/null
+
